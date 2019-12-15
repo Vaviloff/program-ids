8bebb89c64f1ff810152bb153bda156f55513876
vaviloff <vlad@vaviloff.ru>
2019-12-16 00:26:04 +0700

    Correct description

--- test/indexTest.js
+++ test/indexTest.js
@@ -9,7 +9,7 @@ const pagePath = path.join('file:///', __dirname, 'page.html');
 describe('it should get IDs', function testExample() {
   this.timeout(30000);
 
-  it('browses the website', async () => {
+  it('gets the IDs', async () => {
     const ids = await getIds(pagePath);
     expect(ids).to.include.members(['3', '2', '1']);
   });
