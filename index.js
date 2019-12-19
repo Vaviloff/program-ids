0eb08a10c48e2ea6163be0d377189b9422ccd03a
vaviloff <vlad@vaviloff.ru>
2019-12-20 02:16:20 +0700

    Move scrape login to a module and test it

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const { scrape } = require('./lib/scrape.js');
 
 const argv = require('yargs') // eslint-disable-line
   .option('url', {
@@ -22,46 +23,21 @@ if (!argv.proxy) {
   throw Error('Proxy is necessary');
 }
 
-const scrape = async (page, url) => {
-  let pageUrl = url;
-
-  if (!url.includes('/episodes/player')) {
-    pageUrl += '/episodes/player';
-  }
-
-  await page.goto(pageUrl, { waitUntil: 'domcontentloaded' });
-
-  const { ids, next } = await page.evaluate(() => {
-    // Get links of the programs
-    const links = [...document.querySelectorAll('.programme__titles a')];
-    const programIds = links.map((link) => link.href.split('/').pop());
-
-    // Get nextUrl
-    const nextUrl = document.querySelector('.pagination__next a') === null ? false : document.querySelector('.pagination__next a').href;
-
-    return { ids: programIds, next: nextUrl };
-  });
-
-  // @TODO: Send urls to queue
-  console.log(ids);
-
-  if (next) {
-    await page.waitFor(1000 + Math.random() * 2000);
-    return scrape(page, next);
-  }
-
-  return true;
-};
-
 (async () => {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
+  let pageUrl = argv.url;
+
+  if (!pageUrl.includes('/episodes/player')) {
+    pageUrl += '/episodes/player';
+  }
+
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36');
 
-  await scrape(page, argv.url);
+  await scrape(page, pageUrl);
 
   await browser.close();
 })();
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,16 +1,44 @@
 const { expect } = require('chai');
-
+const puppeteer = require('puppeteer');
 const path = require('path');
-
-const getIds = require('../index');
+const { process } = require('../lib/scrape');
 
 const pagePath = path.join('file:///', __dirname, 'page.html');
 
-describe('it should get IDs', function testExample() {
+const opts = {
+  headless: false,
+  slowMo: 100,
+  timeout: 0,
+  args: ['--start-maximized', '--window-size=1366,786'],
+};
+
+before(async () => {
+  global.expect = expect;
+  global.browser = await puppeteer.launch(opts);
+});
+
+after(async () => {
+  await global.browser.close();
+});
+
+describe('it should get IDs', async function testExample() {
   this.timeout(30000);
 
+  let page;
+
+  before(async () => {
+    page = await global.browser.newPage();
+    await page.setViewport({ width: 1366, height: 786 });
+  });
+
+  after(async () => {
+    await page.close();
+  });
+
   it('gets the IDs', async () => {
-    const ids = await getIds(pagePath);
+    const { ids, next } = await process(page, pagePath);
+    console.log(ids);
     expect(ids).to.include.members(['3', '2', '1']);
+    expect(next).to.equal(false);
   });
 });
